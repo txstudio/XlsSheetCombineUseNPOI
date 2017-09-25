8a655b35fb7c5dd42a44577bf1335ad5f0abece7
Shi Hao Wang <st890409@hotmail.com>
2017-09-25 12:23:45 +0800

    合併 EXCEL 工作表範例程式碼
    將多個 EXCEL 檔案的指定工作表合併成新的 EXCEL 檔案

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using XlsSheetProvider;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        const string _stock_etf_path = "../../file/stock_etf.xls";
+        const string _stock_list_path = "../../file/stock_list.xls";
+        const string _stock_list_2_path = "../../file/stock_list_2.xls";
+
+        const string _outPath = "../../file/out.xls";
+
+        static void Main(string[] args)
+        {
+            byte[] _etfBuffer;
+            byte[] _listBuffer;
+            byte[] _list2Buffer;
+            byte[] _outBuffer;
+
+            XlsSheetCombiner _combiner;
+
+            _combiner = new XlsSheetCombiner();
+
+
+            //取得要合併的 EXCEL 檔案
+            _etfBuffer = File.ReadAllBytes(_stock_etf_path);
+            _listBuffer = File.ReadAllBytes(_stock_list_path);
+            _list2Buffer = File.ReadAllBytes(_stock_list_2_path);
+
+
+            //進行工作表合併
+            _combiner.Add(_etfBuffer, 0, "ETF");
+            _combiner.Add(_listBuffer, 0, "上市櫃公司一覽表");
+            _combiner.Add(_list2Buffer, 0, "興貴公司一覽表");
+            _combiner.Save();
+
+
+            //取得合併後的檔案位元陣列
+            _outBuffer = _combiner.XlsContent;
+
+            
+            File.WriteAllBytes(_outPath, _outBuffer);
+
+            
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XlsSheetProvider/XlsSheetCombiner.cs
@@ -0,0 +1,63 @@
+﻿using NPOI.HSSF.UserModel;
+using System;
+using System.IO;
+
+namespace XlsSheetProvider
+{
+    /*
+    * 使用 NPOI 套件將多個
+    *  指定索引 EXCEL (*.xls) 的 Sheet (工作表) 合併成新的 EXCEL(*.xls)
+    *  
+    * 此專案為 NET.Standard
+    *  僅適用於 full .NET Framework 版本
+    */
+
+    /// <summary>提供進行 EXCEL(*.xls) 工作表合併方法</summary>
+    public sealed class XlsSheetCombiner
+    {
+        private HSSFWorkbook _workbook;
+        private Byte[] _buffer;
+
+
+        public XlsSheetCombiner()
+        {
+            this._workbook = new HSSFWorkbook();
+        }
+
+        /// <summary>加入指定 EXCEL (*.xls) 檔案指定的索引 Sheet</summary>
+        /// <param name="buffer">檔案位元陣列</param>
+        /// <param name="sheetIndex">複製的工作表索引</param>
+        /// <param name="sheetName">工作表名稱</param>
+        public void Add(byte[] buffer, int sheetIndex, string sheetName)
+        {
+            using (MemoryStream _stream = new MemoryStream(buffer))
+            {
+                HSSFWorkbook _workbook = new HSSFWorkbook(_stream);
+                HSSFSheet _sheet = _workbook.GetSheetAt(sheetIndex) as HSSFSheet;
+
+                _sheet.CopyTo(this._workbook, sheetName, true, true);
+            }
+        }
+
+        /// <summary>儲存工作表設定</summary>
+        public void Save()
+        {
+            using (MemoryStream _stream = new MemoryStream())
+            {
+                this._workbook.Write(_stream);
+                this._buffer = _stream.ToArray();
+            }
+        }
+
+        /// <summary>取得合併後的 EXCEL 檔案位元陣列（唯讀）</summary>
+        public byte[] XlsContent
+        {
+            get
+            {
+                return this._buffer;
+            }
+        }
+
+
+    }
+}
